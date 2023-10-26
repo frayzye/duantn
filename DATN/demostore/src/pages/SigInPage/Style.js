982816128f7c4cd3da51e75e9e8cba178da1185a
dduong2903 <149073224+dduong2903@users.noreply.github.com>
2023-10-26 19:29:32 +0700

    Update Style.js

--- DATN/demostore/src/pages/SigInPage/Style.js
+++ DATN/demostore/src/pages/SigInPage/Style.js
@@ -5,19 +5,25 @@ export const WrapperContainerLeft = styled.div`
     padding: 40px 45px 24px;
     display: flex;
     flex-direction: column;
-`
 
+`
 export const WrapperContainerRight = styled.div`
     width: 300px;
-    background: linear-gradient(136deg, rgb(240, 248, 255) -1%, rgb(219, 238, 255) 85%);
-    display: flex;
+    background: linear-gradient(13deg, rgb(240, 248, 255) -1%, rgb(255 182 59) 85%);
+    display: block;
+    border-radius: 16px;
     justify-content: center;
-    flex-direction: column;
+    flex-direction: colum;
     align-items: center;
     gap: 4px;
 `
-export const WrapperTextLight = styled.span`
+export const WrapperTextRight = styled.span`
     color: rgb(13, 92, 182);
     font-size: 13px;
-    cursor: pointer;
+`
+export const WrapperTextLeft = styled.div`
+    color: rgb(13, 92, 182);
+    font-size: 20px;
+    display: block;
+    margin: 20px 30px;
 `
